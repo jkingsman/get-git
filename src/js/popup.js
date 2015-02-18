4ad84bf665d99367583323182c1d73d992b022c9
Jack Kingsman <jack.kingsman@gmail.com>
2015-02-17 18:27:44 -0800

    Checkbox is just a status indicator
    Sick and tired of buggy checkbox behavior... solution through removal.

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,30 +1,10 @@
-/*
-** file: js/main.js
-** description: javascript code for "html/main.html" page
-*/
-
-var syncRules;
-
 function init_main() {
     //get the current enabled state and rule list
     chrome.storage.sync.get('status', function (data) {
-        if (typeof data.status === "undefined") {
-            //this is first use; enable by default and save
-            chrome.storage.sync.set({
-                "status": 1
-            });
-            var isEnabled = 1;
-        }
-        else {
-            var isEnabled = parseInt(data.status);
-        }
-
-        //make the switch reflect our current state
-        if (isEnabled) {
-            $('#status').prop("checked", true)
-        }
-        else {
-            $('#status').prop("checked", false)
+        if (data.status) {
+            $("#status").html('<span class="bg-success">Enabled</span>');
+        } else {
+            $("#status").html('<span class="bg-danger">Disabled</span>');
         }
     });
 
@@ -37,17 +17,3 @@ function init_main() {
 
 //bind events to dom elements
 document.addEventListener('DOMContentLoaded', init_main);
-
-//handle enabling or disabling or the extension
-$('#status').change(function () {
-    if ($("#status").prop("checked")) {
-        chrome.storage.sync.set({
-            "status": 1
-        });
-    }
-    else {
-        chrome.storage.sync.set({
-            "status": 0
-        });
-    }
-});
